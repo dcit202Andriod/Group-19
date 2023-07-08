41e77ca29a6d19f01a063e3684e46a37cb6f090c
10961885 <103204489+10961885@users.noreply.github.com>
2023-07-08 14:57:48 +0000

    Update DBManager.java

--- app/src/main/java/com/ulan/timetable/appwidget/Dao/DBManager.java
+++ app/src/main/java/com/ulan/timetable/appwidget/Dao/DBManager.java
@@ -7,9 +7,7 @@ import androidx.annotation.Nullable;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-/**
- * From https://github.com/SubhamTyagi/TimeTable
- */
+
 class DBManager {
 
     private static final AtomicInteger sOpenCounter = new AtomicInteger();
