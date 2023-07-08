e67d41e5e2fad95f1b8962bc67b3fc9a9bf441c4
Edemkh <137008451+Edemkh@users.noreply.github.com>
2023-07-08 15:17:10 +0000

    Update ProfileManagement.java

--- app/src/main/java/com/ulan/timetable/profiles/ProfileManagement.java
+++ app/src/main/java/com/ulan/timetable/profiles/ProfileManagement.java
@@ -1,10 +1,4 @@
-s file is part of GymWenApp.
- *
- *     GymWenApp is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
+
  *     GymWenApp is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
